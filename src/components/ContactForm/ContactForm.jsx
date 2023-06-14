e1be9f302a91b7340b2716bed6118ed9ab9982db
Alexon7 <alenkaalexon@gmail.com>
2023-06-14 21:59:49 +0200

    change pattern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -54,7 +54,7 @@ export const ContactForm = () => {
         <Input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+([\-\' ][a-zA-Zа-яА-Я]+)*$"
+          pattern="^[a-zA-Z\s]+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={inputChange}
@@ -65,7 +65,7 @@ export const ContactForm = () => {
         <Input
           type="tel"
           name="number"
-          pattern="^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
+          pattern="^[0-9]+$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={inputChange}
